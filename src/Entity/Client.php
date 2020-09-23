425a8d7e06b4dc9e8278da1dc0d8c55f0bfcad73
Franck J <45272552+ScytAl3@users.noreply.github.com>
2020-09-23 16:25:04 +0200

    Add image field to Client entity and edit VichUploaderBundle config

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -54,6 +54,18 @@ class Client
     private $url;
 
     /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="client_image", fileNameProperty="imageName")
+     * @Assert\Image(
+     *      maxSize="1M",
+     * )
+     * 
+     * @var File|null
+     */
+    private $imageFile;
+
+    /**
      * @ORM\Column(type="string", length=255)
      */
     private $imageName;
@@ -85,6 +97,31 @@ class Client
         $this->url = $url;
 
         return $this;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->setUpdatedAt(new \DateTimeImmutable);
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
     }
 
     public function getImageName(): ?string
