37098cdd9e2543d26f35b1117c6c4def8226b20c
Franck J <45272552+ScytAl3@users.noreply.github.com>
2020-09-24 15:49:08 +0200

    Create training controller, template and update footer link path

---
+++ src/Controller/TrainingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\TrainingRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TrainingController extends AbstractController
+{
+    /**
+     * @Route("/formations", name="app_training")
+     */
+    public function index(TrainingRepository $repo)
+    {
+        return $this->render('training/index.html.twig', ['trainings' => $repo->findBy([], ['createdAt' => 'DESC'])]);
+    }
+}
