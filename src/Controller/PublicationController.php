d6005c39101f67c27ac7a89fabb8c1d0d2b5a802
Franck J <45272552+ScytAl3@users.noreply.github.com>
2020-10-02 19:50:28 +0200

    Update publication template, nav and footer link path and publication controller

--- src/Controller/PublicationController.php
+++ src/Controller/PublicationController.php
@@ -2,16 +2,17 @@
 
 namespace App\Controller;
 
+use App\Repository\PublicationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PublicationController extends AbstractController
 {
     /**
-     * @Route("/publication", name="publication")
+     * @Route("/publications", name="app_publication")
      */
-    public function index()
+    public function index(PublicationRepository $repo)
     {
-        return $this->render('publication/index.html.twig', []);
+        return $this->render('publication/index.html.twig', ['publications' => $repo->findBy([], ['createdAt' => 'DESC'])]);
     }
 }
