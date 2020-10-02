a46dc3b917cfd4bd7a81b4779e54b8f51c7a9bfa
Franck J <45272552+ScytAl3@users.noreply.github.com>
2020-10-02 15:19:04 +0200

    Create publication controller, template, entity and repository

---
+++ src/Controller/PublicationController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PublicationController extends AbstractController
+{
+    /**
+     * @Route("/publication", name="publication")
+     */
+    public function index()
+    {
+        return $this->render('publication/index.html.twig', []);
+    }
+}
---
+++ src/Entity/Publication.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Traits\Timestampable;
+use App\Repository\PublicationRepository;
+// Link the upload mapping to Training entity
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+// Validates that a particular field (or fields) in a Doctrine entity is (are) unique
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass=PublicationRepository::class)
+ * @ORM\Table(name="publications")
+ * @Vich\Uploadable
+ * @ORM\HasLifecycleCallbacks()
+ * @UniqueEntity("title")
+ */
+class Publication
+{
+    use Timestampable;
+    
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Length(
+     *      min = 3,
+     *      max = 50,
+     *      minMessage = "The title of the publication must be at least {{ limit }} characters long",
+     *      maxMessage = "The title of the publication cannot be longer than {{ limit }} characters",
+     *      allowEmptyString = false
+     * )
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\Length(
+     *      min = 10,
+     *      minMessage = "The description of the publication must be at least {{ limit }} characters long",
+     *      allowEmptyString = false
+     * )
+     */
+    private $description;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="publication_image", fileNameProperty="imageName")
+     * @Assert\Image(
+     *      maxSize="1M",
+     * )
+     * @var File|null
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $imageName;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->setUpdatedAt(new \DateTimeImmutable);
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName(?string $imageName): self
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+}
