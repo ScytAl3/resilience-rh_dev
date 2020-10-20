0f1776920a39490367776ac4aa72816b3cfc97a0
Franck J <45272552+ScytAl3@users.noreply.github.com>
2020-10-20 16:56:49 +0200

    refactoring back to top button logic and css animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -23,23 +23,53 @@ console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
  */
 $(document).ready(function () {
     //Get the button:
-    var mybutton = document.getElementById("scrollTopBtn");
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () { scrollFunction() };
-    
+    const backToTopBtn = document.querySelector("#back-to-top-btn");
+    // When the user scrolls down 300px from the top of the document, show the button
+    window.addEventListener("scroll", scrollFunction);
+
     function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            mybutton.style.display = "block";
+        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+            if (!backToTopBtn.classList.contains("btnEntrance")) {
+                backToTopBtn.classList.remove("btnExit");
+                backToTopBtn.classList.add("btnEntrance");
+                backToTopBtn.style.display = "block";
+            }
         } else {
-            mybutton.style.display = "none";
+            if (backToTopBtn.classList.contains("btnEntrance")) {
+                backToTopBtn.classList.remove("btnEntrance");
+                backToTopBtn.classList.add("btnExit");
+                setTimeout(function () {
+                    backToTopBtn.style.display = "none";
+                }, 250);
+            }
         }
     }
-    
     // When the user clicks on the button, scroll to the top of the document
-    $('#scrollTopBtn').on('click', function () {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    })
+    backToTopBtn.addEventListener("click", smoothScrollBackToTop);
+
+    function smoothScrollBackToTop() {
+        const targetPosition = 0;
+        const startPosition = window.pageYOffset;
+        const distance = targetPosition - startPosition;
+        const duration = 750;
+        let start = null;
+
+        window.requestAnimationFrame(step);
+
+        function step(timestamp) {
+            if (!start) start = timestamp;
+            const progress = timestamp - start;
+            window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+            if (progress < duration) window.requestAnimationFrame(step);
+        }
+    }
+
+    function easeInOutCubic(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t * t + b;
+        t -= 2;
+        return c / 2 * (t * t * t + 2) + b;
+    };
 })
 
 
